a805c9b8e890ee52035b7d4457951d5a7fc5e3e7
Munetsi Zunguzira <dev@jetweb.onmicrosoft.com>
2021-11-02 10:27:52 +0200

    added settings page

--- class.wooclone-admin.php
+++ class.wooclone-admin.php
@@ -16,38 +16,86 @@ class Wooclone_Admin
     private function init_hooks()
     {
         self::$initiated = true;
-        add_action( 'admin_enqueue_scripts', array( __CLASS__, 'wooclone_adminAssets' ) );
-        add_action( 'admin_menu', array( __CLASS__, 'wooclone_adminMenu' ) );
+        add_action('admin_enqueue_scripts', array(__CLASS__, 'wooclone_adminAssets'));
+        add_action('admin_menu', array(__CLASS__, 'wooclone_adminMenu'));
+        add_action('admin_init', array(__CLASS__, 'wooclone_registerSettings'));
+        add_action('rest_api_init', array(__CLASS__, 'wooclone_registerSettings'));
     }
 
-    public static function wooclone_adminMenu() {
+    public static function wooclone_adminMenu()
+    {
         add_menu_page(
-            __( 'Woo Clone', 'wooclone-settings' ),
-            __( 'Woo Clone', 'wooclone-settings' ),
+            __('Woo Clone', 'wooclone-settings'),
+            __('Woo Clone', 'wooclone-settings'),
             'manage_options',
             'wooclone_settings',
-            array( __CLASS__, 'wooclone_menuPage' ),
+            array(__CLASS__, 'wooclone_menuPage'),
             'dashicons-tagcloud',
             6
         );
     }
 
-    public static function wooclone_menuPage() {
-        if ( is_file( plugin_dir_path( __FILE__ ) . 'includes/layout.php' ) ) {
-            include_once plugin_dir_path( __FILE__ ) . 'includes/layout.php';
-        }
-    }
- 
-    public static function wooclone_getSettings() {
-        return get_option( 'wooclone_settings' );
-    }
- 
-    public static function wooclone_adminAssets() {
-        if ( isset( $_GET['page'] ) && ! empty( $_GET['page'] ) && 'wooclone-settings' === $_GET['page'] ) {
- 
+    public static function wooclone_menuPage()
+    {
+        if (is_file(plugin_dir_path(__FILE__) . 'includes/layout.php')) {
+            include_once plugin_dir_path(__FILE__) . 'includes/layout.php';
         }
     }
 
+    public static function wooclone_getSettings()
+    {
+        return get_option('wooclone_settings');
+    }
 
+    public static function wooclone_registerSettings()
+    {
+        add_settings_section(
+            'wooclone_endpoint_section',
+            'Field to add API endpoint',
+            array(__CLASS__, 'wooclone_endpointSection_content'),
+            'wooclone_settings'
+        );
+
+        unset($args);
+        $args = array(
+            'type' => 'input',
+            'subtype' => 'text',
+            'id' => 'wooclone_api_endpoint',
+            'name' => 'wooclone_api_endpoint',
+            'required' => 'true',
+            'get_options_list' => '',
+            'value_type' => 'normal',
+            'wp_data' => 'option',
+        );
+        add_settings_field(
+            'wooclone_api_endpoint',
+            'API Endpoint',
+            array(__CLASS__, 'wooclone_render_settings_field'),
+            'wooclone_settings',
+            'wooclone_endpoint_section',
+            $args
+        );
+
+        register_setting(
+            'wooclone_settings',
+            'wooclone_api_endpoint'
+            );
+
+    }
+    
+    public static function wooclone_render_settings_field (){
+        echo '<input type= "text" />';
+    }
+    public static function wooclone_endpointSection_content ( $args){
+        
+        echo '<p>These settings apply to all Plugin Name functionality.</p>';
+    }
+
+    public static function wooclone_adminAssets()
+    {
+        if (isset($_GET['page']) && !empty($_GET['page']) && 'wooclone-settings' === $_GET['page']) {
+
+        }
+    }
 
 }
--- includes/layout.php
+++ includes/layout.php
@@ -1,3 +1,28 @@
-<div>
-    <h1>Admin Page</h1>
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       munetsizunguzira.com
+ * @since      1.0.0
+ *
+ * @package    Woo Clone
+ * @subpackage Wooclone/includes
+ */
+?>
+
+<div class="wrap">
+		        <div id="icon-themes" class="icon32"></div>  
+		        <h2>Woo Clone Settings</h2>  
+		         <!--NEED THE settings_errors below so that the errors/success messages are shown after submission - wasn't working once we started using add_menu_page and stopped using add_options_page so needed this-->
+				<?php settings_errors(); ?>  
+		        <form method="POST" action="options.php">  
+		            <?php 
+		                settings_fields( 'wooclone_settings' );
+		                do_settings_sections( 'wooclone_settings' ); 
+		            ?>             
+		            <?php submit_button(); ?>  
+		        </form> 
 </div>
